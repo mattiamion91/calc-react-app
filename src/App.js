6d4a74a9efa3865605a1ef16978aaace8908bdf3
Mattia Mion <mattia.mion@gmail.com>
2026-07-02 11:12:39 +0200

    feat: add OperationButton, cleare case, choose operation case

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./style.css"
 import React, { useReducer } from "react";
 import DigitButton from "./DigitButton.js";
+import OperationButton from "./OpearationButton.js";
 
 export const ACTIONS = {
     ADD_DIGIT: "add-digit",
@@ -13,10 +14,28 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
     switch (type) {
         case ACTIONS.ADD_DIGIT:
+            if (payload.digit === "0" && state.currentOperand === "0") return state
+            if (payload.digit === "." && state.currentOperand.includes(".")) return state
             return {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+        case ACTIONS.CHOOSE_OPERATION:
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state
+            }
+            if (state.previousOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null
+
+                }
+            }
+
+        case ACTIONS.CLEAR:
+            return {}
     }
 }
 
@@ -29,21 +48,21 @@ export default function App() {
                 <div className="previous-operand">{previousOperand} {operation}</div>
                 <div className="current-operand">{currentOperand}</div>
             </div>
-            <button className="span-two">AC</button>
+            <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
             <button>DEL</button>
-            <button>/</button>
+            <OperationButton operation="/" dispatch={dispatch} />
             <DigitButton digit="1" dispatch={dispatch} />
             <DigitButton digit="2" dispatch={dispatch} />
             <DigitButton digit="3" dispatch={dispatch} />
-            <button>*</button>
+            <OperationButton operation="*" dispatch={dispatch} />
             <DigitButton digit="4" dispatch={dispatch} />
             <DigitButton digit="5" dispatch={dispatch} />
             <DigitButton digit="6" dispatch={dispatch} />
-            <button>+</button>
+            <OperationButton operation="+" dispatch={dispatch} />
             <DigitButton digit="7" dispatch={dispatch} />
             <DigitButton digit="8" dispatch={dispatch} />
             <DigitButton digit="9" dispatch={dispatch} />
-            <button>-</button>
+            <OperationButton operation="-" dispatch={dispatch} />
             <DigitButton digit="." dispatch={dispatch} />
             <DigitButton digit="0" dispatch={dispatch} />
             <button className="span-two">=</button>
---
+++ src/OpearationButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App.js"
+
+export default function operationButton({ dispatch, operation }) {
+    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>{operation}</button>
+}
